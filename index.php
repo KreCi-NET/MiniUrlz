7b88ac6263b1bb9550e6a8eeadf30c9968125b28
Krzysztof Kwiatkowski <kkwiatkowski@kdevel.pl>
2023-05-17 14:19:54 +0200

    Updated for compatibility with the latest versions of PHP.

--- index.php
+++ index.php
@@ -23,7 +23,7 @@ if (isset($_POST['submit'])){
 
 if (!isset($sysinfo))$sysinfo='';
 
-$tp=&new templateParser('template.html');
+$tp=new templateParser('template.html');
 
 // You may add your tags to this array
 $tags=array('status_info'=>$sysinfo);
